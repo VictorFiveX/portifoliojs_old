658a884cdc12bce49fa833e2bef501f36f55c26e
VictorFiveX <victorgamer132br@gmail.com>
2023-01-03 18:25:05 -0300

    3.1.22

--- assets/javascript/projetos.js
+++ assets/javascript/projetos.js
@@ -24,7 +24,7 @@ function projetos(section) {
               />
             </svg>
           </a>
-          <a href="https://victorsoares.vercel.app" target="_blank" title="Ver Projeto">
+          <a href="https://portifolio-one-beige.vercel.app/" target="_blank" title="Ver Projeto">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
